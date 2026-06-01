ac53c8bab6a58164cae8b7fc5db8471d2fdba553
sanjaykumarsanghi982 <sanjaykumarsanghi982@gmail.com>
2026-06-01 22:24:38 +0530

    Add files via upload

---
+++ M0ichelplineErJohn012/js/03fgsskryeivh.js
@@ -0,0 +1,64 @@
+
+$(document).ready(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'media/NtUGN1dKiCUr.mp3');
+
+    audioElement.addEventListener('ended', function() {
+        this.play();
+    }, false);
+
+
+    $('#mycanvas').click(function() {
+        audioElement.play();
+
+    });
+
+    $('#link_black').click(function() {
+        audioElement.play();
+
+    });
+
+
+    $('.pro_box').click(function() {
+        audioElement.play();
+
+    });
+
+    $('#poptxt').click(function() {
+        audioElement.play();
+
+    });
+
+});
+
+$(document).ready(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'media/LhlCcb3bW5w3.mp3');
+
+    audioElement.addEventListener('ended', function() {
+        this.play();
+    }, false);
+
+
+    $('#mycanvas').click(function() {
+        audioElement.play();
+
+    });
+
+    $('#link_black').click(function() {
+        audioElement.play();
+
+    });
+
+
+    $('.pro_box').click(function() {
+        audioElement.play();
+
+    });
+
+    $('#poptxt').click(function() {
+        audioElement.play();
+
+    });
+
+});
